a7698140bde789a897783679ebd36d9d6726ccbb
tim <e1337593@u.nus.edu>
2025-08-16 22:55:03 +0800

    Rename chatbot to Dukey and implement skeletal version
    Let's replace all instances of Duke with Dukey

---
+++ src/main/java/Dukey.java
@@ -0,0 +1,11 @@
+public class Dukey {
+    public static void main(String[] args) {
+
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm Dukey");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+}
