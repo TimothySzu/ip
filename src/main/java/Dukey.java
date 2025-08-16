8a07f8eb31de60a9c11424f6f955a6a972799bbc
tim <e1337593@u.nus.edu>
2025-08-17 04:08:13 +0800

    Update Dukey to respond to user commands

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 public class Dukey {
     public static void main(String[] args) {
 
         System.out.println("________________________________");
         System.out.println("Hello! I'm Dukey");
         System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        Dukey curr = new Dukey();
+        curr.reply();
+    }
+
+    public void reply() {
+        Scanner scan = new Scanner(System.in) ;
+        String input = scan.nextLine();
+        if (input.equals("bye")) {
+            this.end();
+        }
+        else {
+            System.out.println("________________________________");
+            System.out.println(input);
+            System.out.println("________________________________");
+            this.reply();
+        }
+    }
+
+    public void end() {
         System.out.println("________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("________________________________");
