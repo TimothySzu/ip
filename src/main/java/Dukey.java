ad16a749aea156a045f7791aac1f30589022c63b
tim <e1337593@u.nus.edu>
2025-08-17 17:53:27 +0800

    Modify Dukey to store and display inputs

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 public class Dukey {
-    public static void main(String[] args) {
 
+    String [] arr;
+    int i;
+    
+    public static void main(String[] args) {
         System.out.println("________________________________");
         System.out.println("Hello! I'm Dukey");
         System.out.println("What can I do for you?");
@@ -10,20 +13,38 @@ public class Dukey {
         curr.reply();
     }
 
+    public Dukey() {
+        arr = new String [100];
+        i = 0;
+    }
+
     public void reply() {
         Scanner scan = new Scanner(System.in) ;
         String input = scan.nextLine();
         if (input.equals("bye")) {
             this.end();
         }
+        else if (input.equals("list")) {
+            this.lst();
+            this.reply();
+        }
         else {
             System.out.println("________________________________");
-            System.out.println(input);
+            System.out.println("added:" + " " + input);
             System.out.println("________________________________");
+            arr[i] = input;
+            i++;
             this.reply();
         }
     }
 
+    public void lst() {
+        System.out.println("________________________________");
+        for (int j = 0; j < i; j++) {
+            System.out.println(j + 1 + "." + " " + arr[j]);
+        }
+        System.out.println("________________________________");
+    }
     public void end() {
         System.out.println("________________________________");
         System.out.println("Bye. Hope to see you again soon!");
