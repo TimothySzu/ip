0e406f98512b68089bda14da0b033f84e7520e81
tim <e1337593@u.nus.edu>
2025-09-01 02:21:55 +0800

    Adhere to Coding Standards

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 
 public class Driver {
-
+    
+    /**
+     * Program starts here
+      * @param args
+     */
     public static void main (String [] args) {
         Dukey dukey = new Dukey();
     }
