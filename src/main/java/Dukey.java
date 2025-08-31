35d4dc710977f6b7a3d2af56f460e908ae7320f6
tim <e1337593@u.nus.edu>
2025-09-01 02:17:04 +0800

    Add JavaDoc

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -3,18 +3,27 @@ import Exceptions.DukeyException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/** Dukey class represents chatbot */
 public class Dukey {
 
+    /* storage object to handle interactions with .txt file */
     private Storage storage;
-    private TaskList tasklist;
+    /* list of tasks */
+    private TaskList taskList;
+    /* parser to decide how to respond to user input */
     private Parser parser;
+    /* ui to receive and process user input */
     private Ui ui;
 
+    /**
+     * Initialises Dukey chatbot with the required supporting classes.
+     */
     public Dukey() {
 
-        this.tasklist = new TaskList();
-        this.storage = new Storage(tasklist);
-        this.parser = new Parser(this, tasklist, storage);
+        //Initialise supporting classes
+        this.taskList = new TaskList();
+        this.storage = new Storage(taskList);
+        this.parser = new Parser(this, taskList, storage);
         this.ui = new Ui(parser);
 
         System.out.println("________________________________");
@@ -23,13 +32,15 @@ public class Dukey {
         System.out.println("________________________________");
 
         try {
-            ui.reply(); // Start replying to user input
+            ui.reply(); // Start taking and replying to user input
         } catch (DukeyException exception) {
             System.out.println(exception.getMessage()); // Handle any exceptions
         }
     }
 
-    // End the program
+    /**
+     * Ends program.
+     */
     public void end() {
         System.out.println("________________________________");
         System.out.println("Bye. Hope to see you again soon!");
--- src/main/java/Exceptions/DukeyException.java
+++ src/main/java/Exceptions/DukeyException.java
@@ -1,8 +1,14 @@
 package Exceptions;
 
+/** DukeyException handles specific checked Exceptions */
 public class DukeyException extends Exception {
 
-
+    /**
+     * Creates new DukeyException.
+     * Create checked exception with error message.
+     *
+     * @param message Associated error message.
+     */
     public DukeyException (String message) {
         super(message);
     }
